73c57b44af864f97f6747979d8738c9c73ae02eb
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2024-09-01 23:11:54 +0300

    Correction of layouts according to design

--- app/src/main/java/com/study/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/study/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,8 @@ package com.study.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
