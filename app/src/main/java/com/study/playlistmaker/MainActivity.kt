955143db063207d9e49cfb74c35db40c3de06f4b
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2024-09-01 12:50:04 +0300

    [Homework 2] onClick listeners with toasts for MainActivity buttons

--- app/src/main/java/com/study/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/study/playlistmaker/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.study.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                Toast.makeText(this@MainActivity, "Search button clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Library button clicked", Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Settings button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
