cb47c89309420ab08c85d6719529c33792074fb0
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2024-09-22 15:36:45 +0300

    [Homework 2] Intents for setting buttons

--- app/src/main/java/com/study/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/study/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.study.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -14,6 +16,34 @@ class SettingsActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
+        }
+
+        val shareButton = findViewById<Button>(R.id.share)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, R.string.share_link)
+            }
+            startActivity(shareIntent)
+        }
+
+        val supportButton = findViewById<Button>(R.id.support)
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.support_email))
+                putExtra(Intent.EXTRA_SUBJECT, R.string.support_mail_subject)
+                putExtra(Intent.EXTRA_TEXT, R.string.support_mail_body)
+            }
+            startActivity(supportIntent)
+        }
+
+        val agreementButton = findViewById<Button>(R.id.agreement)
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(getString(R.string.agreement_link))
+            }
+            startActivity(agreementIntent)
         }
     }
 }
