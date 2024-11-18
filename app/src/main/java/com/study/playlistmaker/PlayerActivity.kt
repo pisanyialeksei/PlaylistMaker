6a58e2487f7fdd6958357ae585eca2103e5b4a81
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2024-11-18 21:35:00 +0200

    Player activity

---
+++ app/src/main/java/com/study/playlistmaker/PlayerActivity.kt
@@ -0,0 +1,13 @@
+package com.study.playlistmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+
+}
