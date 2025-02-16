f258a49e3aac101e8eaab8b36427f5bec6deb2c6
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2025-02-16 19:13:42 +0200

    Fragments' view models

--- app/src/main/java/com/study/playlistmaker/library/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/study/playlistmaker/library/ui/fragment/FavoritesFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.study.playlistmaker.databinding.FragmentFavoritesBinding
+import com.study.playlistmaker.library.ui.view_model.FavoritesViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding!!
+
+    private val favoritesViewModel: FavoritesViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/study/playlistmaker/library/ui/fragment/PlaylistsFragment.kt
+++ app/src/main/java/com/study/playlistmaker/library/ui/fragment/PlaylistsFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.study.playlistmaker.databinding.FragmentPlaylistsBinding
+import com.study.playlistmaker.library.ui.view_model.PlaylistsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
 
     private var _binding: FragmentPlaylistsBinding? = null
     private val binding get() = _binding!!
+
+    private val playlistsViewModel: PlaylistsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
