0af4cd026eb205ffc503e96caaee21579ea879b9
Aleksei Pisanyi <pisanyialeks@alohabrowser.com>
2025-02-23 14:56:15 +0200

    Apply androidx.navigation.safeargs plugin in other way (due to some false gradle/IDE error)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.androidx.navigation.safeargs)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("androidx.navigation.safeargs")
 }
 
 android {
