cc575274b21cb53c39bd94ffc9270c699ce1d389
Aleksei Pisanyi <pisanyy95@yandex.ru>
2025-03-16 16:08:22 +0200

    Make track genre nullable due to the bug

--- app/src/main/java/com/study/playlistmaker/search/domain/model/Track.kt
+++ app/src/main/java/com/study/playlistmaker/search/domain/model/Track.kt
@@ -10,7 +10,7 @@ data class Track(
     val artworkUrl100: String,
     val collectionName: String?,
     val releaseDate: String,
-    val primaryGenreName: String,
+    val primaryGenreName: String?,
     val country: String,
     val previewUrl: String,
 ) {
