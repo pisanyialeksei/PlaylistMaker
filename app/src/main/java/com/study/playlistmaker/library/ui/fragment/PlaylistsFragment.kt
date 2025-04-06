ecb27a062fd8f5b8d6b0db42f8897d96fe3bfd30
Aleksei Pisanyi <pisanyy95@yandex.ru>
2025-04-06 15:46:11 +0300

    New playlist layout

--- app/src/main/java/com/study/playlistmaker/library/ui/fragment/PlaylistsFragment.kt
+++ app/src/main/java/com/study/playlistmaker/library/ui/fragment/PlaylistsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.study.playlistmaker.R
 import com.study.playlistmaker.databinding.FragmentPlaylistsBinding
 import com.study.playlistmaker.library.ui.view_model.PlaylistsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,6 +24,14 @@ class PlaylistsFragment : Fragment() {
     ): View {
         _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.newPlaylistButton.setOnClickListener {
+            findNavController().navigate(R.id.action_libraryFragment_to_newPlaylistFragment)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/study/playlistmaker/library/ui/view_model/NewPlaylistViewModel.kt
@@ -0,0 +1,7 @@
+package com.study.playlistmaker.library.ui.view_model
+
+import androidx.lifecycle.ViewModel
+
+class NewPlaylistViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
